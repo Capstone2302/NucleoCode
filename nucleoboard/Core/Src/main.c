7598b614f8a09553277cdf55cea0c54914cb1568
Ashli <forbesashli@gmail.com>
2024-02-21 14:23:56 -0800

    pwm fix

--- nucleoboard/Core/Src/main.c
+++ nucleoboard/Core/Src/main.c
@@ -300,7 +300,7 @@ static void MX_TIM3_Init(void)
     Error_Handler();
   }
   /* USER CODE BEGIN TIM3_Init 2 */
-
+  HAL_TIM_PWM_Start(&htim3, TIM_CHANNEL_1);
   /* USER CODE END TIM3_Init 2 */
   HAL_TIM_MspPostInit(&htim3);
 
